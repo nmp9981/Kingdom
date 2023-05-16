e9f7823ccdedc72486eb9431cbe7739717ae1a29
Whiteant1 <chamingy1@gmail.com>
2023-05-17 03:03:17 +0900

    몬스터에 닿으면 목숨 잃음

---
+++ Kingdom/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLife : MonoBehaviour
+{
+    Rigidbody2D PlayerRgbd;
+    public int life = 3;
+    public Text life_text;
+
+    void Start()
+    {
+        life=3;
+        PlayerRgbd = GetComponent<Rigidbody2D>();
+        life_text.text = "Life: "+ life;
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.tag == "Monster"){
+            life--;
+            life_text.text = "Life: "+ life;
+        }
+    }
+}
